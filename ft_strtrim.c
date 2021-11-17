3b1954876f94fa33615cde26de7d9d7ea642c48f
Byeongju Kim <byeokim@c7r4s3.42seoul.kr>
2021-11-17 20:13:38 +0900

    test01

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: byeokim <byeokim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 19:03:46 by byeokim           #+#    #+#             */
-/*   Updated: 2021/11/17 19:18:23 by byeokim          ###   ########.fr       */
+/*   Updated: 2021/11/17 20:13:28 by byeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,12 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
+	size_t	end;
+	char	*result;
+	
+	start = 0;
+	end = ft_strlen(s1);
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	while (s1[start] != '\0' && ft_strchr(set, s1[start]) != '\0')
+		start++;
